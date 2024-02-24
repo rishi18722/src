d7e2c43bffe352b92e279e68e29b8df41cd3e507
rishi18722 <reeshavchowdhury18722@gmail.com>
2024-02-24 18:03:03 +0530

    added config.rviz , integra

--- arm2/launch/display.launch.py
+++ arm2/launch/display.launch.py
@@ -4,11 +4,15 @@ import os
 from ament_index_python.packages import get_package_share_path
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
-
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     urdf_path = os.path.join(get_package_share_path('arm2'),
                              'urdf','assem2.urdf')
+    
+    rviz_config_path = os.path.join(get_package_share_path('arm2'),
+                                    'rviz','urdf_config.rviz')
 
     robot_description = ParameterValue(Command(['xacro ',urdf_path]),value_type=str)
 
@@ -25,21 +29,30 @@ def generate_launch_description():
 
     rviz2_node = Node(
         package="rviz2",
-        executable="rviz2"
+        executable="rviz2",
+        arguments=['-d', rviz_config_path]
     )
 
-    #spawn = Node( 
-    #    package='ros_gz_sim', 
-    #    executable='create', 
-    #    arguments=[ '-name', 'my_bot', '-topic', 'robot_description', ], 
-    #    output='screen'
-    #)
+    gazebo_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_path('gazebo_ros'),'launch'),"/gazebo.launch.py"])
+        
+    )
+
+    spawn_node = Node( 
+        package='gazebo_ros', 
+        executable='spawn_entity.py', 
+        arguments=['-topic', 'robot_description','-entity', 'arm2'], 
+        output='screen'
+    )
+
 
 
     return LaunchDescription([
         robot_state_publisher_node,
-        joint_state_publisher_gui_node,
+        #joint_state_publisher_gui_node,
         rviz2_node,
-        #spawn
+        gazebo_node,
+        spawn_node
 
     ])
